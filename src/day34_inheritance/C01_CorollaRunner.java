c57ad1d0ee5cf77f0842d60c4c9bffa29f67c249
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-04 14:18:50 +0200

    version

--- src/day34_inheritance/C01_CorollaRunner.java
+++ src/day34_inheritance/C01_CorollaRunner.java
@@ -1,4 +1,23 @@
 package day34_inheritance;
 
 public class C01_CorollaRunner {
+    public static void main(String[] args) {
+
+          /*
+            Bir class'dan obje olusturdugumuzda
+            sadece o class'daki ozelliklere degil
+            o class'in parent'larinda olan ozelliklere de sahip olur
+
+            bu ornegi dusunursek
+            biz sadece C09_Corolla() corolla constructor'ini calistirdik
+
+            extends keyword kullanan bir class'da bulunan
+            HER CONSTRUCTOR once parent class'daki
+            constructor'in calismasini saglayacak sekilde
+            ozel bir mekanizmaya sahiptir
+
+            Her new obje olusturdugumuzda bir constructor çalıstırmıs oluyoruz !!
+
+         */
+    }
 }
--- src/day34_inheritance/C02_GParent.java
+++ src/day34_inheritance/C02_GParent.java
@@ -1,4 +1,7 @@
 package day34_inheritance;
 
 public class C02_GParent {
+    public static void main(String[] args) {
+
+    }
 }
