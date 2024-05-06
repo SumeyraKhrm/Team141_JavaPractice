bd9e30ed31a4eff13b3ec8a26a79c47cb0de5ea3
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-06 21:20:07 +0200

    version

--- src/day38_abstractClasses_Interfaces/C05_ConcreteChildClass.java
+++ src/day38_abstractClasses_Interfaces/C05_ConcreteChildClass.java
@@ -1,4 +1,21 @@
 package day38_abstractClasses_Interfaces;
 
-public class C05_ConcreteChildClass {
+public class C05_ConcreteChildClass extends C04_AbstractParent{
+
+    //concrete (abstract olmayan)
+
+    public int method2() {
+        return 10;
+    }
+
+    public void method5() {
+        System.out.println("Child Method 5");
+    }
+   /*
+        Abstract silsileden sonra gelen
+        ILK CONCRETE class
+        parent class'larda concrete olmayan / concrete yapilmayan
+        tum abstract method'lari uyarlamak ZORUNDADIR
+
+    */
 }
