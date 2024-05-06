bdea43da36acd71e0543fa89d9534d5473b028f1
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-06 17:30:30 +0200

    version

--- src/day37_abstraction/C01_KuralciOlmayanParent.java
+++ src/day37_abstraction/C01_KuralciOlmayanParent.java
@@ -3,4 +3,16 @@ package day37_abstraction;
 public class C01_KuralciOlmayanParent {
 
 
+    public void method1(){
+
+    }
+
+    public void method2(){
+
+    }
+
+    public void method3(){
+
+    }
+
 }
--- src/day37_abstraction/C04_AbstractClassinChildi.java
+++ src/day37_abstraction/C04_AbstractClassinChildi.java
@@ -1,4 +1,19 @@
 package day37_abstraction;
 
-public class C04_AbstractClassinChildi {
+public class C04_AbstractClassinChildi extends C03_KismiKuralciAbstractParent {
+
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    // Class 'C04_AbstractClassinChildi'
+    // must either be declared abstract
+    // or implement abstract method 'method1()' in 'C03_KismiKuralciAbstractParent'
+
 }
